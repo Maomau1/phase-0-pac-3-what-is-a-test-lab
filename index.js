32e296492cb715bf1a9285a3e2e2c848b7033533
maurice diong <mdiong00@gmail.com>
2022-08-21 15:46:15 -0400

    commit tree 71e02fae831361800011930b2fbb8fa22555c5f2

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 34;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!");
+  }
+/*
+  sayHello();
+
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe");
+  }
+  sayHelloToGuadalupe();
+
+  function sayHelloTo(firstname) {
+    console.log(`Hello, ${firstname}!`);
+  }
+ 
+  sayHelloTo("Sam");
+  sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"
+  sayHelloTo("Jane"); // "Hello, Jane!"
+  sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+  sayHelloTo(1); // "Hello, 1!"
+
+  sayHelloTo(["Sam","Penelope","Sam"]);
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+  say("hi", "buddy");
+*/
+  function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+    
+  }
+  console.log(say("Howdy", "partner"));
